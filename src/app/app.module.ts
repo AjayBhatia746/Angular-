a367eb3b4161345b2be4b49f47f87a9eaeeeeb0a
AjayBhatia746 <bhatiaa746@gmail.com>
2019-09-02 09:13:26 +0530

    =Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ServersComponent } from './servers/servers.component';
+// import { ServersComponent } from './servers/servers.component';
+import { MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment.prod';
+import { OtpComponent } from './otp/otp.component';
+
 @NgModule({
   declarations: [
+    
     AppComponent,
     
-    ServersComponent,
-    
+    OtpComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
     BrowserModule,
     FormsModule,
-    HttpClientModule
-    
+    HttpClientModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/otp/otp.component.ts
+++ src/app/otp/otp.component.ts
@@ -1,45 +1,49 @@
-import { WindowService } from './../window.service';
+//import { environment } from './../../environments/environment';
 import { Component, OnInit } from '@angular/core';
+import { WindowService } from './../window.service';
 import * as firebase from 'firebase'
-
-@Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
-  
-})
+import { environment } from '../../environments/environment.prod';
 export class Phonenumber{
-country:string;
-area:string;
-prefix:string;
-line:string;
-
-get e164(){
-  const num=this.country+this.area+this.prefix+this.line
-  return `+${num}`
-}
-}
-export class ServersComponent implements OnInit {
+  country:string;
+  area:string;
+  prefix:string;
+  line:string;
   
+  get e164(){
+    const num=this.country+this.area+this.prefix+this.line
+    return `+${num}`
+  }
+  }
+@Component({
+  selector: 'app-otp',
+  templateUrl: './otp.component.html',
+  styleUrls: ['./otp.component.css']
+})
+export class OtpComponent implements OnInit {
   windowRef:any;
   phoneNumber=new Phonenumber
   verificationCode:string;
   user:any;
   constructor(private win:WindowService){}
   ngOnInit() {
+    firebase.initializeApp(environment.firebase);
     this.windowRef=this.win.windowRef
     this.windowRef.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
     this.windowRef.recaptchaVerifier.render()
   }
   sendLoginCode(){
     var phoneNumber = this.phoneNumber.e164;
+    console.log(phoneNumber)
 var appVerifier = this.windowRef.recaptchaVerifier;
+console.log("otp")
 firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
     .then(function (confirmationResult) {
       // SMS sent. Prompt user to type the code from the message, then sign the
       // user in with confirmationResult.confirm(code).
+      console.log("otp Sent")
       this.windowRef.confirmationResult = confirmationResult;
     }).catch(function (error) {
+      console.log("Error not SENT")
       console.log(error)
     });
   }
@@ -54,6 +58,5 @@ this.windowRef.confirmationResult.confirm(this.verificationCode).then(function (
  console.log({error:error})
 });
   }
-  }
 
-
+}
